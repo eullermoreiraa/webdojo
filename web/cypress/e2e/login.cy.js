11dbc60775fd03f7cfa52a30ef927ba91c271b55
Euller Moreira <eullermmoreira@gmail.com>
2025-06-19 00:26:13 -0300

    18.06

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -2,7 +2,9 @@ describe('Login', ()=> {
     it('Deve logar com sucesso', ()=> {
         cy.visit('http://localhost:3000')
 
-        cy.get('#email')
-        cy.get('#password')
+        cy.get('#email').type('papito@webdojo.com')
+        cy.get('#password').type('katana123')
+
+        cy.contains('button', 'Entrar').click()
     })
 })
